0506d5dfaf0bb2e8a93dfb357af73ad53b83990d
alexSh14 <alex.1486@mail.ru>
2023-07-27 22:33:07 +0700

    Добавьте файлы проекта.

---
+++ OOP Home work4 (Dictionary)/Program.cs
@@ -0,0 +1,35 @@
+﻿Dictionary<string, Action> commands = new Dictionary<string, Action>();
+commands["привет"] = SayHello;
+commands["время"] = ShowTime;
+commands["пока"] = SayGoodbye;
+
+Console.WriteLine("Введите команду (привет, время, пока):");
+string input = Console.ReadLine().ToLower();
+
+if (commands.ContainsKey(input))
+{
+    Action command = commands[input];
+    command();
+}
+else
+{
+    Console.WriteLine("Неверная команда!");
+}
+
+Console.ReadLine();
+    
+
+    static void SayHello()
+{
+    Console.WriteLine("Hello!");
+}
+
+static void ShowTime()
+{
+    Console.WriteLine("Текущее время: " + DateTime.Now.ToString("HH:mm:ss"));
+}
+
+static void SayGoodbye()
+{
+    Console.WriteLine("Bye!");
+}
